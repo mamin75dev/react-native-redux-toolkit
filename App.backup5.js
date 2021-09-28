98bd43371bd34abae7e8084ae4ce357b7a0649c3
amin <mamin75.dev@gmail.com>
2021-09-28 17:25:19 +0330

    app-tour

---
+++ App.backup5.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, StyleSheet, NativeModules} from 'react-native';
+
+const {ShakeDetectionModule} = NativeModules;
+
+function App() {
+  return <AppContent />;
+}
+
+const AppContent = () => {
+  React.useEffect(() => {
+    ShakeDetectionModule.shakeDetection();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Shake device</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 40,
+  },
+});
+``
